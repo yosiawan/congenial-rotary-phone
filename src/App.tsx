d0377801fe32c13ab245e212496081aae07e9a16
yosiawan <yosiasetiawan@outlook.com>
2021-12-11 12:21:51 +0700

    create body

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Toolbar from "@mui/material/Toolbar";
+import {
+  ROUTE_DRIVER_MGMT,
+  ROUTE_MENU_A,
+  ROUTE_MENU_C,
+} from "./constants/routes";
+import ComingSoon from "./ComingSoon/ComingSoon";
 
 function App() {
   const [sidebarOpenInMobile, setSidebarOpenInMobile] = useState(false);
@@ -16,7 +24,16 @@ function App() {
           isOpenInMobile={sidebarOpenInMobile}
           toggleOpenInMobile={sidebarToggleHandler}
         />
-        <div className="app-body"></div>
+        <div className="app-body">
+          <Toolbar />
+          <Routes>
+            <Route path="/" element={<div>Welcome to Shipper</div>} />
+            <Route path={ROUTE_DRIVER_MGMT} element={<ComingSoon />} />
+            <Route path={ROUTE_MENU_A} element={<ComingSoon />} />
+            <Route path={ROUTE_MENU_C} element={<ComingSoon />} />
+            <Route path="*" element={<div>Page Not Found</div>} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
---
+++ src/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,3 @@
+export default function ComingSoon() {
+  return <div> <h1>Coming Soon</h1></div>
+}
